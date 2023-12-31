80eb2fc87c14a4ebe5eb552ad1df584886f07780
Vicktor007 <vicktord007@gmail.com>
2023-12-31 18:06:27 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173", "https://e-shop-frontend-green.vercel.app"],
+    origin: ["http://localhost:5173", "https://test-e-shop-api.vercel.app"],
     credentials: true
   }));
 
